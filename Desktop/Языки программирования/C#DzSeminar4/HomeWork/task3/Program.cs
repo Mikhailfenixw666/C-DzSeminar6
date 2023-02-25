c628a29d72f4230fb966abaa9e0a22bb77f28171
Mikhailfenixw666 <124437837+Mikhailfenixw666@users.noreply.github.com>
2023-02-26 02:44:59 +0300

    домашнее задание к семинару№4 задача№3

---
+++ Desktop/Языки программирования/C#DzSeminar4/HomeWork/task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+int[] GenerateArray(int size)
+{
+
+int[] array = new int[size];
+
+Random rand = new Random();
+
+for(int i = 0; i < size; i++)
+{
+array[i] = rand.Next(1, 34);
+}
+return array;
+}
+
+var myArray = GenerateArray(8);
+
+System.Console.WriteLine("[" + string.Join(", ", myArray) + "]");
