99f10546ecad50bd2a16850d394b9772033481bc
Mikhailfenixw666 <124437837+Mikhailfenixw666@users.noreply.github.com>
2023-02-26 02:46:05 +0300

    домашнее задание к семинару№4 задача№1

---
+++ Desktop/Языки программирования/C#DzSeminar4/HomeWork/task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int numberA = ReadInt("Число A: ");
+int numberB = ReadInt("Число B: ");
+ToDegree(numberA, numberB);
+
+void ToDegree(int a, int b)
+{
+ int result = 1;
+ for (int i = 1; i <= b; i++)
+    {
+ result = result * a;
+    }
+ Console.WriteLine(result);
+}
+
+// Функция ввода
+int ReadInt(string message)
+{
+ Console.WriteLine(message);
+ return Convert.ToInt32(Console.ReadLine());
+}
