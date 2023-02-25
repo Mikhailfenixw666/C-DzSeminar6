cfd9084c677293cf8ce3c7fa1891dcad907f1b9f
Mikhailfenixw666 <124437837+Mikhailfenixw666@users.noreply.github.com>
2023-02-25 14:35:06 +0300

    Cеминар №5

---
+++ Desktop/Языки программирования/Семинар №5/Demo/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+
+int[] array = new int[12];
+Random rand = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(-9, 10); // [-9, 10) > -9 до 9
+}
+
+System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+
+int sumPositive = 0;
+int sumNegative = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        sumPositive += array[i]; // sumPositive = sumPositive + array[i]
+    }
+    else
+    {
+        sumNegative += array[i];
+    }
+}
+
+System.Console.WriteLine($"Сумма положительных чисел: {sumPositive}");
+System.Console.WriteLine($"Сумма отрицательных чисел: {sumNegative}");
